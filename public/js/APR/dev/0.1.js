58718b2948eb6855d150649ed3a5beeace408293
Alexis Puga <alexispugaruiz@hotmail.com>
2018-11-08 02:58:02 -0500

    New code in APR

--- public/js/APR/dev/0.1.js
+++ public/js/APR/dev/0.1.js
@@ -645,6 +645,23 @@
 		
 		},
 		/**
+		 * A cross-browser solution to get the pressed key on a keyboard
+		 * event.
+		 * 
+		 * @param Event e Some event.
+		 * @return {number|string} Any of e.key, e.code, e.which or w.keyCode
+		 *
+		 * @example
+		 * document.addEventListener('keypress', function (e) {
+		 *     if (/(Enter|13)/.test(getPressedKey(e))) {
+		 *         console.log('Enter');
+		 *     }
+		 * });
+		 */
+		'getPressedKey' : function (e) {
+			return e.key || e.code || e.which || e.keyCode;
+		},
+		/**
 		 * A custom function to append the created element.
 		 * 
 		 * @typedef {function} APR~load_handler
