386a3ddd522fe0f03f8838e1e4078465977fb8f1
Alexis Puga Ruíz <alexispugaruiz@hotmail.com>
2019-06-02 02:49:30 -0500

    MAINT: Better configurations for builds

--- build/config.js
+++ build/config.js
@@ -1,7 +1,90 @@
+// Note: All files are relative to the Gruntfile location.
+
+const pkg = require('../package.json');
+
+const options = {
+	// Banner for build files.
+	'banner': '/*! ' + pkg.title + ' v' + pkg.version +
+	' - (c) ' + pkg.author + '.' +
+	' - license: ' + pkg.license +
+	' */\n',
+	// Paths for the builds.
+	getPath (key) {
+		
+		const path = './build';
+		const pathTest = path + '/test';
+		const paths = {
+			'base': path,
+			'src': path + '/src',
+			'test': pathTest,
+			'test-tap': pathTest + '/tap',
+			'test-tape': pathTest + '/tape',
+			'test-server': pathTest + '/server',
+			'mutableProduction': path + '/dist',
+			// Path for finished files.
+			'production': './dist'
+		};
+
+		// Returns everything.
+		if (key === '*') {
+			return paths;
+		}
+
+		return (key ? paths[key] : path);
+
+	},
+	// Handler for replacements of variables during the build.
+	replaceVars (code) {
+
+		// Data for variables.
+		const vars = {
+			'CORE_VERSION': pkg.version
+		};
+
+		// Matches: // content %{key}%.
+		const regexp = /\/\/(.+)\%\{(.+)\}\%/g;
+
+		// Replaces `regexp` with vars[key] if vars[key] exists,
+		// otherwise it replaces `regexp` with an empty string.
+		const handler = ($0, content, key) => {
+			return (key in vars
+				? content + vars[key]
+				: ''
+			);
+		};
+
+		return code.replace(regexp, handler);
+
+	},
+	// Modifies a filename to match the unit-test one.
+	getUnitTestFilename (filename, type) {
+
+		// Renames:
+		// ./someModule.js -> ./someModule.build.test.js
+		if (type === 'build') {
+			return filename + '.build.test.js';
+		}
+
+		// Renames: ./src/lib/someModule.js -> {
+		//     build -> lib-someModule.build.test.js
+		//     default -> lib-someModule.test.js
+		// }
+		return filename
+			.replace(/\.?\/?src\//, '')
+			.replace(/\//g, '-')
+			.replace(/(\.js|$)/, '.test.js');
+	
+	}
+
+};
+
 module.exports = {
+	'options': options,
 	'browser': {
-		'files': ['./src/browser.js'].concat([
-			'polyfills',
+		'files': [
+			'./src/lib/polyfills.js',
+			'./src/browser.js'
+		].concat([
 			'var/body',
 			'var/DNT',
 			'var/ELEMENT_NAMESPACES',
--- src/lib/var/self.js
+++ src/lib/var/self.js
@@ -19,12 +19,12 @@ define(function () {
 		 * @property {string} staticOriginUrl The origin of the URL that serves APR's files.
 	 * @property {APR~self_setFileUrl} setFileUrl Sets the url of APR's files.
 	 */
-	return Object.create({
+	return {
 		'originUrl': 'https://www.aprservices.com',
 		'staticOriginUrl': 'https://www.code.aprservices.com',
 		'setFileUrl': function (name, ext, version) {
 			return this.staticOriginUrl + '/' + ext + '/' + name + '/dev/' + version + '.' + ext + '?nocache=true'
 		}
-	});
+	};
 
 });
