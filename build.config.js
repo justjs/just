a4d54da483c683718349b3f214bd67d4ff37dd47
Alexis Puga Ruíz <alexispuga@pm.me>
2019-09-11 21:42:11 -0500

    DEP: Remove "elementNamespaces" and move it externally (to APRElement)

--- build.config.js
+++ build.config.js
@@ -180,7 +180,6 @@ module.exports = {
 		'polyfillsSrc': ['./src/lib/polyfills.js'],
 		'files': [
 			'browser',
-			'lib/var/elementNamespaces',
 			'lib/access',
 			'lib/check',
 			'lib/defaults',
