9a7edb029887fcd39613a32787b3fb7364fe76fc
Alexis Puga Ruíz <alexispuga@pm.me>
2019-10-14 19:51:00 -0500

    BUG: "browser.test.js" is being skipped in browser
    Fix it.

--- test/tape/browser.test.js
+++ test/tape/browser.test.js
@@ -1,6 +1,6 @@
 var test = require('tape');
 var browser = require('../../src/browser');
-var options = {'skip': true};
+var options = {'skip': typeof window === 'undefined'};
 
 test('/browser.js', options, function (t) {
 
