516e34c5bfef3ed3797e3a75299285bd7633a7e0
Alexis Puga Ruíz <alexispuga@pm.me>
2019-10-17 01:28:07 -0500

    TST [Jest]: Add more tests for "@lib/getPressedKey.test.js"

--- test/jest/lib/getPressedKey.test.js
+++ test/jest/lib/getPressedKey.test.js
@@ -3,10 +3,31 @@ var getPressedKey = require('@lib/getPressedKey');
 describe('@lib/getPressedKey.js', function () {
 
     it('Should get one of the supported variants for a key ' +
-		'when a keyboard event is fired.', function () {
+        'when a keyboard event is fired.', function () {
 
         /** Doesn't matter if it's not an Event. */
-        expect(getPressedKey({'key': 'Enter'}) + '').toMatch(/^(Enter|13)$/);
+        expect(getPressedKey({'key': 'Enter'}) + '').toBe('Enter');
+        expect(getPressedKey({'which': 13})).toBe(13);
+        expect(getPressedKey({'keyCode': 13})).toBe(13);
+
+    });
+
+    it('Should return the most recent implementation first.', function () {
+
+        expect(getPressedKey({
+            'key': 'key',
+            'which': 'which',
+            'keyCode': 'keyCode'
+        })).toBe('key');
+
+        expect(getPressedKey({
+            'which': 'which',
+            'keyCode': 'keyCode'
+        })).toBe('which');
+
+        expect(getPressedKey({
+            'none-supported': true
+        })).toBe(void 0);
 
     });
 
