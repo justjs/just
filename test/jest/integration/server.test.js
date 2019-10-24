2d70523b8d9266c9a5435ac73e93ce20d88da057
Alexis Puga Ruíz <alexispuga@pm.me>
2019-10-24 00:06:00 -0500

    TST: Add aliases to integration tests

--- test/jest/integration/server.test.js
+++ test/jest/integration/server.test.js
@@ -4,15 +4,24 @@ describe('Server bundle', function () {
 
     test.each([
         ['access'],
+        ['prop'],
         ['check'],
+        ['is'],
         ['defaults'],
+        ['from'],
         ['defineProperties'],
+        ['defProps'],
         ['defineProperty'],
+        ['defProp'],
         ['eachProperty'],
+        ['eachProp'],
         ['isEmptyObject'],
+        ['emptyObj'],
         ['parseUrl'],
         ['stringToJSON'],
-        ['toObjectLiteral']
+        ['toJSON'],
+        ['toObjectLiteral'],
+        ['toObj']
     ])('Should contain %p as a property.', function (property) {
 
         expect(serverBundle.just).toHaveProperty(property);
