c97660d8d928d2d53a8e82d75da8874abf9039b1
Alexis Puga Ruíz <alexispuga@pm.me>
2019-11-29 15:26:08 -0600

    DOC: Add SpaceMono fonts to a separate folder and include a license there
    DOC: Add bash templates into "docs/tmpl"
    
    Add "available-versions-table" template.
    
    ENH: Add Subresource Integrity (SRI) capabilities
    
    Add "./bin/generateSRI".
    Generate SRI for some example.
    
    DOC: Update "docs/static/versioned/index.html" to show download links
    
    DOC: Add live-reloading mode for docs
    
    Add "doc:dev" script.
    
    ENH: Add gulp
    
    Add gulpfile.js
    
    ENH: Add live-server
    
    BUG: Only the first version is showing up on docs
    
    Use quotes and arrays when possible on binaries.
    
    BUG: Only local versions are showing up on docs
    
    Use git to fetch all remote tags and use those instead.
    
    DOC: Use getAvailableVersions once and fetch tags only on production
    
    Move getAvailableVersions to a new file and fetch remote tags only when
    NODE_ENV is equal to 'production'.
    
    MAINT: Use dashed-lowercase names for binaries instead of camelCase ones
    
    MAINT: Set NODE_ENV to "production" in Travis
    
    STY: Change paddings and sizes in non-versioned index
    
    BUG: Things referring to "version" are actually git tags
    
    Rename "version" with "tag" in binaries.
    
    MAINT: Return non-release candidates from get-available-versions
    
    Ignore things using a dash.
    
    DOC: Fix links in versioned index.html
    
    Change color of links to gray.
    
    DOC: Fix link to image
    
    DOC: Remove color for navigation bar in jsdoc docs
    
    DOC: Add styles to table-wrapper
    
    DOC: Update non-versioned/index.html
    
    Fix bad name for npm installation.
    Add "versioning" section.
    Rename "available flavors" to "about bundles"
    Add polyfills.min.js file info.
    
    DOC: Add page footer
    
    DOC: Use percentages when possible
    
    DOC: Add default footnote for bundles
    
    DOC: Show x2 logos on retina display
    
    DOC: Add viewport meta-tag
    
    DOC: Add styles for small screens
    
    DOC: Use percentages when possible
    
    DOC: Change font
    
    DOC: Add all favicons
    
    DOC: Use SpaceMono font
    
    DOC: Add color
    
    DOC: Add banner and remove colors
    
    MAINT: Remove extra file
    
    DOC: Adjust for larger screens and change design for #home page
    
    Move text to the left in #home.
    Move the banner to the bottom of #home.
    Change gold texts to silver.
    ...
    
    DOC: Add a simpler page for versioned index
    
    Add a 512x512 logo
    
    DOC: Add #State section and load travis badge after
    
    DOC: Add some styles to template
    
    DOC: Remove unused js files
    
    DOC: Add preload and prefetch links
    
    Update README.md

--- README.md
+++ README.md
@@ -2,6 +2,4 @@
 
 *The core for all your javascript projects.*
 
-Just is a library with a bunch of useful and heavily-used js helpers for your browser and server developments.
-
-Just avoid you rewriting the most random code used in your developments without actually depending on multiple libraries. That's our mission.
+**Just** is a library with a bunch of useful and heavily-used js helpers for your browser and server developments.
---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var liveServer = require('live-server');
+var spawn = require('child_process').spawn;
+var gulp = require('gulp');
+var pkg = require('./package.json');
+var Docs = (function () {
+
+    function document (done) {
+
+        var cmd = spawn('./bin/document', ['v' + pkg.version], {
+            'cwd': __dirname
+        });
+
+        cmd.stdout.on('data', function (data) { console.log(data.toString()); });
+        cmd.stderr.on('data', function (data) { console.error(data.toString()); });
+        cmd.on('close', function (code) { done(); });
+
+    }
+
+    function serve (done) {
+
+        liveServer.start({
+            'root': 'docs/public',
+            'ignore': ['docs/public/**/browser', 'docs/public/**/server', '**/*.temp']
+        });
+
+        done();
+
+    }
+
+    function stopServing (done) { liveServer.shutdown(); done(); }
+    function watch (done) { gulp.watch(['docs/static', 'docs/tmpl', 'bin'], {'ignoreInitial': false}, document); }
+
+    return {
+        'default': gulp.series(serve, watch, stopServing),
+        'watch': watch,
+        'document': document
+    };
+
+})();
+
+exports.default = Docs.default;
