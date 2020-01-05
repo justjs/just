d0034b00767ca24ee36c008bc0d62f6996114a00
Alexis Puga Ruíz <alexispuga@pm.me>
2020-01-05 12:55:08 -0600

    MAINT: Replace "!!" with Boolean in src/lib/isWindow.js

--- src/lib/isWindow.js
+++ src/lib/isWindow.js
@@ -10,7 +10,7 @@
  */
 function isWindow (object) {
 
-    return !!(object === window
+    return Boolean(object === window
 		|| object instanceof Object
 		&& object.document
 		&& object.setInterval
