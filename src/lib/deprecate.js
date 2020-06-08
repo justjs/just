ff39e810defee2749af86919dcec1b9885a914ad
Alexis Puga Ruíz <alexispuga@pm.me>
2020-06-08 05:10:44 -0500

    ENH: Add just.deprecate()
    DOC: Add docs
    
    TST: Add unit tests

---
+++ src/lib/deprecate.js
@@ -0,0 +1,28 @@
+/**
+ * Show a warning in the console, or throw an Error if called.
+ *
+ * @since 1.0.0-rc.23
+ * @namespace
+ * @memberof just
+ * @param {string} member - The member's name. E.g.: ".deprecate()"
+ * @param {string} type - "warning" or "error". Otherwise it logs the error using console.error().
+ * @param {?object} options
+ * @param {?string} options.since - Some string.
+ * @param {?string} options.message - A message to append to the default message.
+ * @throws {Error} If <var>type</var> is set to "error".
+ */
+function deprecate (member, type, options) {
+
+    var opts = Object.assign({}, options || {});
+    var message = ('just' + member + ' is deprecated' +
+		(opts.since ? ' since ' + opts.since : '') + '. ' +
+		(opts.message || '')
+    ).trim();
+
+    if (type === 'warning') { console.warn(message); }
+    else if (type === 'error') { throw new Error(message); }
+    else { console.error(message); }
+
+}
+
+module.exports = deprecate;
