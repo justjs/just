f440e3c1c473d2817239e9680a8f35ab2b9e4570
Alexis Puga Ruíz <alexispuga@pm.me>
2020-06-16 04:12:05 -0500

    TST: Update integration tests
    Add "createElement", "create", "request" and "ajax" members for browser
    build.
    Append library to the document using a <script> element and read
    "window.just", instead of using "require()".

--- test/jest/integration/browser.test.js
+++ test/jest/integration/browser.test.js
@@ -4,51 +4,80 @@ var fs = require('fs');
 
 describe('Core', function () {
 
+    function appendLibrary (src) {
+
+        var text = fs.readFileSync(src);
+        var script = document.createElement('script');
+
+        script.id = src;
+        script.innerHTML = text;
+        document.head.appendChild(script);
+
+    }
+
+    function removeLibrary (id) {
+
+        var script = document.getElementById(id);
+
+        script.parentNode.removeChild(script);
+
+    }
+
     test.each([
-        ['@dist/browser/core.js'],
-        ['@dist/browser/core.min.js']
+        ['dist/browser/core.js'],
+        ['dist/browser/core.min.js']
     ])('%s should contain all members.', function (src) {
 
-        var core = require(src).just;
+        expect(function () {
 
-        expect(core).toEqual(expect.objectContaining({
-            'deprecate': expect.any(Function),
-            'access': expect.any(Function),
-            'prop': expect.any(Function),
-            'check': expect.any(Function),
-            'is': expect.any(Function),
-            'ClassList': expect.any(Function),
-            'defaults': expect.any(Function),
-            'from': expect.any(Function),
-            'Define': expect.any(Function),
-            'Def': expect.any(Function),
-            'defineProperties': expect.any(Function),
-            'defProps': expect.any(Function),
-            'defineProperty': expect.any(Function),
-            'defProp': expect.any(Function),
-            'eachProperty': expect.any(Function),
-            'eachProp': expect.any(Function),
-            'findElements': expect.any(Function),
-            'el': expect.any(Function),
-            'getRemoteParent': expect.any(Function),
-            'parent': expect.any(Function),
-            'isEmptyObject': expect.any(Function),
-            'emptyObj': expect.any(Function),
-            'isTouchSupported': expect.any(Function),
-            'supportsTouch': expect.any(Function),
-            'isWindow': expect.any(Function),
-            'loadElement': expect.any(Function),
-            'load': expect.any(Function),
-            'LocalStorage': expect.any(Function),
-            'addEventListener': expect.any(Function),
-            'on': expect.any(Function),
-            'parseUrl': expect.any(Function),
-            'stringToJSON': expect.any(Function),
-            'toJSON': expect.any(Function),
-            'toObjectLiteral': expect.any(Function),
-            'toObj': expect.any(Function),
-            'parseJSON': expect.any(Function)
-        }));
+            appendLibrary(src);
+
+            expect(window.just).toEqual(expect.objectContaining({
+                'deprecate': expect.any(Function),
+                'access': expect.any(Function),
+                'prop': expect.any(Function),
+                'check': expect.any(Function),
+                'is': expect.any(Function),
+                'ClassList': expect.any(Function),
+                'defaults': expect.any(Function),
+                'from': expect.any(Function),
+                'Define': expect.any(Function),
+                'Def': expect.any(Function),
+                'defineProperties': expect.any(Function),
+                'defProps': expect.any(Function),
+                'defineProperty': expect.any(Function),
+                'defProp': expect.any(Function),
+                'eachProperty': expect.any(Function),
+                'eachProp': expect.any(Function),
+                'findElements': expect.any(Function),
+                'el': expect.any(Function),
+                'getRemoteParent': expect.any(Function),
+                'parent': expect.any(Function),
+                'isEmptyObject': expect.any(Function),
+                'emptyObj': expect.any(Function),
+                'isTouchSupported': expect.any(Function),
+                'supportsTouch': expect.any(Function),
+                'isWindow': expect.any(Function),
+                'loadElement': expect.any(Function),
+                'load': expect.any(Function),
+                'LocalStorage': expect.any(Function),
+                'addEventListener': expect.any(Function),
+                'on': expect.any(Function),
+                'parseUrl': expect.any(Function),
+                'stringToJSON': expect.any(Function),
+                'toJSON': expect.any(Function),
+                'toObjectLiteral': expect.any(Function),
+                'toObj': expect.any(Function),
+                'parseJSON': expect.any(Function),
+                'createElement': expect.any(Function),
+                'create': expect.any(Function),
+                'request': expect.any(Function),
+                'ajax': expect.any(Function)
+            }));
+
+            removeLibrary(src);
+
+        }).not.toThrow();
 
     });
 
