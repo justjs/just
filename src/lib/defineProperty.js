b126abb23e2d52fc13ad249592857a724e22afeb
Alexis Puga Ruíz <alexispuga@pm.me>
2020-06-19 08:30:59 -0500

    DOC: Update defineProperty docs

--- src/lib/defineProperty.js
+++ src/lib/defineProperty.js
@@ -1,24 +1,31 @@
 /**
  * Alternative to <var>Object.defineProperty</var> with more enhancements.
  *
- * If <var>object</var> contains any other key that's not a valid attribute for a
- * {@link propertyDescriptor|property descriptor} the value WON'T be used
- * as a property descriptor. I.e:
- * <code>
- * defineProperty({}, 'property', {
- *     value: 1,
- *     other: 'value'
- * }).property; // > {value: 1, other: 'value'}
- * </code>
- *
- * Note: Empty objects will be considered values rather than property descriptors.
+ * <br>
+ * <aside class='note'>
+ *     <h3>A few things to consider (see examples):</h3>
+ *     <ul>
+ *         <li>If <var>object</var> contains invalid {@link propertyDescriptor|property descriptor attributes},
+ *             the value WON'T be used as a property descriptor.</li>
+ *         <li>Empty objects will be considered values.</li>
+ *     </ul>
+ * </aside>
  *
  * @namespace
  * @memberof just
  * @throws <var>Object.defineProperty</var> exceptions.
  * @param {!object} object - The target.
  * @param {string} key - Key for the property.
- * @param {!object} [value={value}] - A {@link propertyDescriptor} or some value.
+ * @param {!object} [value={'value': value}] - A {@link propertyDescriptor|property descriptor} or some value.
+ * @example <caption>Define a property using a value.</caption>
+ * defineProperty({}, 'a', 1); // Same as Object.defineProperty({}, 'a', {'value': 1})
+ *
+ * @example <caption>Define a property using a {@link propertyDescriptor|property descriptor}.</caption>
+ * defineProperty({}, 'a', {'writable': true}); // Same as Object.defineProperty({}, 'a', {'writable': true})
+ *
+ * @example <caption>Define a property with an empty object.</caption>
+ * defineProperty({}, 'a', {}); // Same as Object.defineProperty({}, 'a', {'value': {}});
+ *
  * @return <var>object</var>.
  */
 function defineProperty (object, key, value) {
