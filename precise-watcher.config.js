c5ecbe6bdcf472e46e2dd7c8a2b3d19a870875e6
Alexis Puga <alexispuga@pm.me>
2022-03-02 12:44:06 -0600

    ENH: Setup live-server
    Update precise-watcher config to remove, recreate, and serve the "docs/public"
    directory.

--- precise-watcher.config.js
+++ precise-watcher.config.js
@@ -1,1 +1,18 @@
-module.exports = {};
+var SERVER_DIR = 'docs/public';
+
+module.exports = {
+	'src': [{
+		'pattern': SERVER_DIR,
+		'on': 'ready',
+		'run': [{
+			'cmd': 'rm',
+			'args': [SERVER_DIR, '-R']
+		}, {
+			'cmd': 'mkdir',
+			'args': [SERVER_DIR]
+		}, {
+			'cmd': 'live-server',
+			'args': [SERVER_DIR],
+		}]
+	}]
+};
