0ac54a369c8cc0f7a9b1e3826aea4032cdf03b1a
Alexis Puga <alexispuga@pm.me>
2022-03-02 22:27:34 -0600

    STY: Fix linting issues
    Run `npm run style .`.

--- precise-watcher.config.js
+++ precise-watcher.config.js
@@ -1,33 +1,33 @@
 var SERVER_DIR = 'docs/public';
 
 module.exports = {
-	'src': [{
-		'pattern': SERVER_DIR,
-		'on': 'ready',
-		'run': [{
-			'cmd': 'rm',
-			'args': [SERVER_DIR, '-R']
-		}, {
-			'cmd': 'mkdir',
-			'args': [SERVER_DIR]
-		}, {
-			'cmd': 'live-server',
-			'args': [SERVER_DIR],
-		}]
-	}, {
-		'pattern': ['docs/static', 'docs/tmpl'],
-		'on': ['change'],
-		'run': [{
-			'cmd': 'bin/document',
-			'args': ['vx.x.x', '--run-jsdoc=false'],
-		}]
-	}, {
-		// @TODO Merge this into the previous task.
-		'pattern': ['src', 'bin', 'precise-watcher.config.js'],
-		'on': ['ready', 'change'],
-		'run': [{
-			'cmd': 'bin/document',
-			'args': ['vx.x.x', '--run-jsdoc=true']
-		}]
-	}]
+    'src': [{
+        'pattern': SERVER_DIR,
+        'on': 'ready',
+        'run': [{
+            'cmd': 'rm',
+            'args': [SERVER_DIR, '-R']
+        }, {
+            'cmd': 'mkdir',
+            'args': [SERVER_DIR]
+        }, {
+            'cmd': 'live-server',
+            'args': [SERVER_DIR],
+        }]
+    }, {
+        'pattern': ['docs/static', 'docs/tmpl'],
+        'on': ['change'],
+        'run': [{
+            'cmd': 'bin/document',
+            'args': ['vx.x.x', '--run-jsdoc=false'],
+        }]
+    }, {
+        // @TODO Merge this into the previous task.
+        'pattern': ['src', 'bin', 'precise-watcher.config.js'],
+        'on': ['ready', 'change'],
+        'run': [{
+            'cmd': 'bin/document',
+            'args': ['vx.x.x', '--run-jsdoc=true']
+        }]
+    }]
 };
