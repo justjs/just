859152030a662ca300bf8f54904bf09767ed18b7
Alexis Puga <alexispuga@pm.me>
2022-03-04 05:48:25 -0600

    STY: Fix linting issues
    Run `npm run style .`.

--- nunjucks.config.js
+++ nunjucks.config.js
@@ -1,14 +1,14 @@
 var env = process.env;
 var NODE_ENV = env.NODE_ENV;
-var npm_package_version = env.npm_package_version;
+var npmPackageVersion = env.npm_package_version;
 
 module.exports = {
-	'render': {
-		'context': {
-			'env': NODE_ENV,
-			'production': NODE_ENV === 'production',
-			'development': NODE_ENV === 'development',
-			'version': npm_package_version
-		}
-	}
+    'render': {
+        'context': {
+            'env': NODE_ENV,
+            'production': NODE_ENV === 'production',
+            'development': NODE_ENV === 'development',
+            'version': npmPackageVersion
+        }
+    }
 };
