b946e501fabbcd4a187726fd5d9616b21aaed65d
Alexis Puga <alexispuga@pm.me>
2022-03-05 02:34:37 -0600

    ENH: Add getIntegrity function on nunjucks config
    Run `npm run build` if the given file doesn't exist.
    Generate hash using bin/generate-sri binary.

--- nunjucks.config.js
+++ nunjucks.config.js
@@ -1,8 +1,28 @@
+var fs = require('fs');
+var path = require('path');
+var childProcess = require('child_process');
+var spawnSync = childProcess.spawnSync;
 var env = process.env;
 var NODE_ENV = env.NODE_ENV;
 var npmPackageVersion = env.npm_package_version;
 var production = NODE_ENV === 'production';
 var development = NODE_ENV === 'development';
+
+function build () {
+
+    const {stdout} = spawnSync('npm run build', []);
+
+    return `${stdout}`;
+
+}
+
+function generateHash (filename) {
+
+    const {stdout} = spawnSync(path.resolve('bin/generate-sri'), [filename]);
+
+    return `${stdout}`;
+
+}
 
 module.exports = {
     'render': {
@@ -13,7 +33,19 @@ module.exports = {
             'version': (production
                 ? npmPackageVersion
                 : 'x.x.x'
-            )
+            ),
+            'getIntegrity': function (src) {
+
+                var filename = src;
+                var hash;
+
+                if (!fs.existsSync(filename)) { build(); }
+
+                hash = generateHash(filename);
+
+                return hash;
+
+            }
         }
     }
 };
