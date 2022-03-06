a2ed644b7a6c12111a5a582a03f451e42e16fa1f
Alexis Puga <alexispuga@pm.me>
2022-03-06 06:39:15 -0600

    TST: Add e2e test for raw scripts in docs
    Add test/e2e/docs.test.js.
    Test if raw scripts in docs are working.

---
+++ test/e2e/docs.test.js
@@ -0,0 +1,89 @@
+/**
+ * This test checks if raw scripts shown in the docs
+ * are fully working by inserting them on a page.
+ *
+ * This makes sure [integrity] values are working, and [src]
+ * values are set to a CDN url.
+ */
+var el = function (selector) { return [].slice.call(document.querySelectorAll(selector)); };
+var rawScripts = el('#browser-setup code').map(function (element) { return element.textContent });
+var loadScript = function (rawScript) {
+
+	var container = document.body;
+
+	return new Promise(function (resolve, reject) {
+
+		// Raw scripts won't be executed by using .innerHTML. That's why we create a Node instead.
+		var script = document.createElement('script');
+		var attributes = rawScript.match(/\w+="[^"]*"/g);
+
+		attributes.forEach(function (attribute) {
+
+			var parts = attribute.split('=');
+			var name = parts[0];
+			var value = parts[1].slice(1, -1); // Remove quotes.
+
+			if (name === 'src') {
+
+				// Instead of making an external request, check the file locally.
+				value = value.replace(/https\:\/\/cdn\.jsdelivr\.net\/npm\/@just-js\/just@([^\/]+)\/(.+)/, '$2');
+
+			}
+
+			this.setAttribute(name, value);
+
+		}, script);
+
+		script.onload = function () {
+
+			if ('just' in window) { resolve(); }
+			else { reject(new Error('`just` is not defined.')); }
+
+		};
+		script.onerror = function (e) { reject(e); };
+
+		container.appendChild(script);
+
+	});
+
+};
+
+window.karmaCustomEnv = {
+	'execute': function (karma) {
+
+		if (!rawScripts.length) {
+
+			return karma.result({
+				'success': false,
+				'suite': [],
+				'log': [new Error('Scripts are not being shown.')]
+			});
+
+		}
+
+		Promise
+			.all(rawScripts.map(loadScript))
+			.then(function () {
+
+				karma.result({
+					'success': true
+				});
+
+			})
+			.catch(function (e) {
+
+				karma.result({
+					'success': false,
+					'suite': [],
+					'log': [e]
+				});
+
+			})
+			.finally(function () {
+
+				karma.complete({});
+
+			});
+
+	}
+};
