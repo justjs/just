3d06421b509a5b342b2817bd4dc1e52953e61a74
Alexis Puga <alexispuga@pm.me>
2022-03-17 05:59:25 -0500

    DOC: Redirect to previous versions on <select> change
    Redirect to old urls (/browser/).

--- docs/static/versioned/js/index.js
+++ docs/static/versioned/js/index.js
@@ -9,8 +9,22 @@
     versions.addEventListener('change', function (e) {
 
         var version = this.value;
+        var pathname = location.pathname;
+        /**
+         * Versions below 1.1.0 generated documentation
+         * in /{browser,server}/index.html.
+         *
+         * Starting from version 1.1.0, documentation is generated
+         * under /browser/{core,just}/index.html and server/index.html.
+         */
+        var newUrl = (/^1\.[01]\./.test(version)
+            // Redirect to the full version.
+            ? pathname.replace(/(browser)\/just/, '$1')
+            // Use as it is.
+            : pathname
+        ).replace(/v[^\/]+/, 'v' + version);
 
-        location.href = '/v' + version + '/browser/';
+        location.href = newUrl;
 
     });
 
